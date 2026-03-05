4812c7f7672fd102d9986802e833f9f0cd151844
minhee513 <alsgml513@naver.com>
2026-03-05 18:04:11 +0900

    Spring DI 실습1 제출

---
+++ src/main/java/com/example/springframework5_project/SpringFramework5ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springframework5_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramework5ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFramework5ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springframework5_project/SpringFramework5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springframework5_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework5ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
